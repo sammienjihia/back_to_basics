a293307831cd6cbd0bf5837579b6a1180311ee32
sammienjihia <dilaninjihia@gmail.com>
2018-12-20 09:25:01 +0300

    fixed bug on find_pairs_that_add_up_to()

--- technical_question.py
+++ technical_question.py
@@ -3,17 +3,23 @@ Given a integer array and a number k, output pairs that sum up to k
 """
 
 def find_pairs_that_add_up_to(arr, k):
-
+ 	
+ 	# remove duplicates
 	num_sets = set(arr)
 
+	# convert the set into an iterable
 	nums = list(num_sets)
 
+	# A list to hold our tuples
 	num_list = []
 
 	for num in nums:
 
+		# get the compliment
 		complement = k-num
 		
+		# if complement is in the set list, then add the complement 
+		# and num in the num_list and remove the num from the set list
 		if complement in nums:
 			num_list.append((complement, num))
 			nums.remove(num)
@@ -21,32 +27,7 @@ def find_pairs_that_add_up_to(arr, k):
 	print(num_list)
 			
 
-def find_pairs_that_add_up_to2(arr, k):
-	num_sets = set(arr)
-
-	nums = list(num_sets)
-
-	tuples_list = []
-
-	for i in range(0, len(nums)-1):
-		for n in range(0, len(nums)-1):
-			if nums[i] == nums[n]:
-				pass
-
-			elif (nums[i] + nums[n]) ==  k:
-				
-				tuples_list.append((nums[i], nums[n]))
-
-
-
-
-		
-
-
-
-	print(tuples_list)
 
 
 find_pairs_that_add_up_to([1,3,5,3, 2,7,2,6,8,4], 8)
 
-find_pairs_that_add_up_to2([1,3,5,3, 2,7,2,6,8,4], 8)
