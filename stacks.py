220c9cd23bb829d9e11b12373c100235c87e2975
sammienjihia <dilaninjihia@gmail.com>
2018-12-27 14:05:35 +0300

    No comment

---
+++ stacks.py
@@ -0,0 +1,15 @@
+"""
+This is a really simple implementation of a stack
+NB: A stack has a LIFO implementation, this means the last item into the stack is the first item to be removed 
+from the stack
+
+"""
+
+arr = []
+
+# Add item to a stack
+arr.append()
+
+# Remove item from stack
+arr.pop()
+
