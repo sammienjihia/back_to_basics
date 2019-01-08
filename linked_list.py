4d3914681d8b709290a0564d1c2c2cfd079b08ed
sammienjihia <dilaninjihia@gmail.com>
2019-01-08 22:53:31 +0300

    implemented a linked list using the oop paradigm

--- linked_list.py
+++ linked_list.py
@@ -8,6 +8,14 @@ https://www.youtube.com/watch?v=6sBsF13n5ig
 
 https://github.com/AtotheY/YoutubeTutorials/blob/master/Introductions/linkedListOnlyNodes.py
 
+doubly linked lists: https://www.youtube.com/watch?v=sDP_pReYNEc
+
+stack abuse linked list: https://stackabuse.com/python-linked-lists/
+
+lucidprogramming doubly linked list: https://www.youtube.com/watch?v=8kptHdreaTA
+
+single linked list: https://dbader.org/blog/python-linked-list
+
 
 """
 
@@ -15,7 +23,7 @@ https://github.com/AtotheY/YoutubeTutorials/blob/master/Introductions/linkedList
 """
 In the below code, we've simply created a class that has a value and a nexNode attribute
 """
-class LinkedList:
+class LinkedListNode:
 	"""docstring for ClassName"""
 	def __init__(self, value, nextNode=None):
 
@@ -24,10 +32,10 @@ class LinkedList:
 
 
 # To create a node, we simply pass in a value
-node1 = LinkedList(3)
-node2 = LinkedList(4)
-node3 = LinkedList(5)
-node4 = LinkedList(6)
+node1 = LinkedListNode(3)
+node2 = LinkedListNode(4)
+node3 = LinkedListNode(5)
+node4 = LinkedListNode(6)
 
 # Then we connect the nodes together
 node1.nextNode = node2
@@ -45,4 +53,27 @@ while True:
 		break
 
 	currentNode = currentNode.nextNode
+
+
+class LinkedList():
+
+	def __init__(self, head=None):
+		self.head = head
+
+	def insert(self, value):
+		node = LinkedListNode(value)
+
+		if self.head is None:
+			self.head = node
+			return
+
+		currentNode = self.head
+
+		while True:
+			if currentNode.nextNode is None:
+				currentNode.nextNode = node
+				break
+			currentNode = currentNode.nextNode
+
+		
 		
