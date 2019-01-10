50234818b7baea3a2da225eb271c129234845b51
sammienjihia <dilaninjihia@gmail.com>
2019-01-10 21:32:59 +0300

    Implementing a binary search tree in python

---
+++ binary_search_tree.py
@@ -0,0 +1,25 @@
+
+
+class Node:
+	def __init__(self, data):
+
+		self.data = data
+		self.lef_child = None
+		self.right_child= None
+
+
+class BsTree:
+	def __init__(self, ):
+		pass
+
+
+	def insert(self, data):
+		pass
+
+	def search(self, data):
+		pass
+
+	def find(self, data):
+		pass
+
+	
