c31ff5b735b237d107fc94917272e1a80ad6bbe3
sammienjihia <dilaninjihia@gmail.com>
2019-01-11 21:02:22 +0300

    Implementing the bibary search tree in python

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -10,11 +10,31 @@ class Node:
 
 class BsTree:
 	def __init__(self, ):
-		pass
+		self.root = None
 
 
 	def insert(self, data):
-		pass
+		if self.root is None:
+			self.root = Node(data)
+		else:
+			self.__insert(data, self.root)
+
+	def __insert(self, data, current_node):
+		if data < current_node.data:
+			if current_node.left is None:
+				current_node.left = Node(data)
+			else:
+				self.__insert(data, current_node.left)
+
+		elif data> current_node.data:
+			if current_node.right is None:
+				current_node.right = Node(data)
+
+			else:
+				self.__insert(data, current_node.right)
+
+		else:
+			pass
 
 	def search(self, data):
 		pass
