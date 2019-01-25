b88ce5d310b3b9c760d2bf420de51a973d684424
sammienjihia <dilaninjihia@gmail.com>
2019-01-25 16:20:31 +0300

    correct bug: Implemented the graph ADT in python

--- graphs.py
+++ graphs.py
@@ -8,17 +8,14 @@ class Node:
 		self.value = value
 
 		# the neighbours to the node
-		self.neighbours = list()
+		self.neighbours = []
 
 	# adding a neighbour to a list of neighbours to a node
 
 	def add_neighbours(self, nnode):
-
-		if nnode not in self.neighbours:
+		# To be included in the neighbors list, don't include the node itslsef, don't repeat a node
+		if nnode not in self.neighbours and nnode.value is not self.value:
 			self.neighbours.append(nnode)
-
-			# sort the list of neigbours
-			self.neighbours.sort()
 
 			return True
 
@@ -28,45 +25,72 @@ class Node:
 
 class Graph:
 
+	def __init__(self):
+		# Key value pairs where the Key is the inserted Node, and the Value is the list of the Node's neighbours
+		self.nodes = {}
+		self.edges = {}
+
 	# The nodes will be a dictionary where the key is the value of a node and the value will
 	# be the list of it's neighbours
 
-	nodes = {}
 
 	def add_node(self, node):
-		# Check if the given node is an insatnce of the Node 
+		# Check if the given node is already represented in the graph dictionary
 
-		if isinstance(node, Node) and node.value not in self.nodes:
-			# add the node into the nodes dict with the node value as the key
-			self.nodes[node.value] = node
+		if node not in self.nodes :
+			self.nodes[node] = node.neighbours
 
 			return True
-
 		else:
 			return False
 
 
-	# an edge is a connection between two nodes
-	def add_edge(self, node1, node2):
-		# first check if both nodes are in the nodes dictionary
 
-		if node1.value in self.nodes and node2.value in self.nodes:
-			for key, value in self.nodes.items():
-				if key == node1.value:
-					value.add_neighbours(node1)
+		# if isinstance(node, Node) and node.value not in self.nodes:
+		# 	# add the node into the nodes dict with the node value as the key
+		# 	self.nodes[node.value] = node
 
-				if key == node2.value:
-					value.add_neighbours(node2)
+		# 	return True
+
+		# else:
+		# 	return False
+
+	def add_edge(self, node1, node2, name):
+
+		if node1 != node2:
+			if node1 in self.nodes and node2 in self.nodes:
+
+				node1.add_neighbours(node2)
+
+				node2.add_neighbours(node1)
+
+				self.edges[name] = (node1, node2)
+			else:
+				return False
 
 			return True
-
 		else:
 			return False
 
+	def get_edges(self):
+		for key, value in self.edges.items():
+			print("Edge {}: {}, {}".format(key, value[0].value, value[1].value))
+
+
+		
 
 	def print_graph(self):
-		for key in sorted(list(self.nodes.keys())):
-			print(str(key) + str(self.nodes[key].neighbours))
+
+		for key, value in self.nodes.items():
+			print(key.value, [x.value for x in value] )
+		
+		
+
+
+
+
+
+	
 
 
 
@@ -74,33 +98,67 @@ g = Graph()
 
 node1 = Node(1)
 node2 = Node(2)
-node3 = Node(2)
-
-node1.add_neighbours(node2)
-
-node1.add_neighbours(node3)
-
-node2.add_neighbours(node1)
-
-node2.add_neighbours(node1)
-
-node3.add_neighbours(node2)
-
-node3.add_neighbours(node1)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
 
 print("************* adding a node *******************")
 print(g.add_node(node1))
 print(g.add_node(node2))
 print(g.add_node(node3))
+print(g.add_node(node4))
+print(g.add_node(node5))
 print("***************** adding a node")
 print(" ")
 print("############# adding an edge ##########")
-print(g.add_edge(node1, node2))
-print(g.add_edge(node1, node3))
-print(g.add_edge(node3, node2))
+print(g.add_edge(node1, node2, "A"))
+print(g.add_edge(node1, node3, "B"))
+print(g.add_edge(node3, node2, "C"))
+print(g.add_edge(node2, node4, "D"))
+print(g.add_edge(node4, node3, "E"))
+print(g.add_edge(node5, node3, "F"))
 print("########### adding an edge ###########3")
 
 (g.print_graph())
+
+(g.get_edges())
+
+
+
+"""
+Graph data structure from: Data Structures and Algorithms in Python [Goodrich, Tamassia & Goldwasser 2013-03-18] 
+"""
+
+class Vertex:
+	def __init__(self, x):
+		self._name = x
+
+	def name(self):
+		# Return the name associated with this vertex.(The name of this vertex)
+		return self._name
+
+class Edge:
+	def __init__(self, origin, destination):
+		self.origin = origin
+		self.destination = destination
+
+	def edges(self):
+		# return a tuple of vertices representing an edge
+		return(self.origin, self.destination)
+
+	def opposite(self, v):
+		# Return the vertex that is opposite v on tthis edge
+		return self.destination if v is self.origin else self.origin
+
+class Graph:
+
+	def __init__(self, directed=False):
+
+		self.origin = {}
+
+	def vertex_count(self):
+		return len(self.origin)
+
 
 
 
